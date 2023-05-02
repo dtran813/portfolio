dce9f542f2c7f775ec289e7b86ab80554bfcebc2
dtran <dtran13899@gmail.com>
2023-05-01 22:01:08 -0600

    Change aspect ratio for icon

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,45 +2,44 @@ import styles from './Hero.module.css';
 import { motion } from 'framer-motion';
 
 export default function Hero() {
+  const animation = {
+    animate: {
+      opacity: [0, 0.5, 1],
+      y: [-25, 0],
+    },
+    transition: {
+      duration: 0.5,
+    },
+  };
   return (
     <section id="hero" className={styles.heroWrapper}>
       <motion.p
-        animate={{
-          opacity: [0, 0.5, 1],
-          y: [-25, 0],
-        }}
-        transition={{
-          duration: 0.5,
-        }}
+        animate={animation.animate}
+        transition={animation.transition}
         className={styles.mdText}
       >
         Hi, my name is &nbsp;
       </motion.p>
       <div>
-        <h1 className={styles.name}>
-          <SplitText text={'Duc Tran.'} role="heading" />
+        <h1
+          className={styles.name}
+          role="heading"
+          aria-label="Duc Tran."
+          aria-level={1}
+        >
+          <SplitText text={'Duc Tran.'} />
         </h1>
       </div>
       <motion.p
-        animate={{
-          opacity: [0, 0.5, 1],
-          y: [-25, 0],
-        }}
-        transition={{
-          duration: 0.5,
-        }}
+        animate={animation.animate}
+        transition={animation.transition}
         className={styles.lgText}
       >
         I love to see all the beautiful things on the web.
       </motion.p>
       <motion.p
-        animate={{
-          opacity: [0, 0.5, 1],
-          y: [-25, 0],
-        }}
-        transition={{
-          duration: 0.5,
-        }}
+        animate={animation.animate}
+        transition={animation.transition}
         className={styles.lgText}
       >
         And soon, you will see me build some of them.
@@ -55,12 +54,11 @@ export default function Hero() {
 
 interface Props {
   text: string;
-  role: string;
 }
 
-function SplitText({ text, role }: Props) {
+function SplitText({ text }: Props) {
   return (
-    <span aria-label={text} role={role}>
+    <span>
       {text.split('').map((char, index) => {
         return (
           <motion.span
